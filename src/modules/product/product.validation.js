ef80531a0305208a95de5568ae002eb962f986f9
Dharmesh Vala <dharmesh.vala@zeuslearning.com>
2025-12-07 13:05:20 +0530

    endpoint added for getting featured products

--- src/modules/product/product.validation.js
+++ src/modules/product/product.validation.js
@@ -38,7 +38,10 @@ const addProductValidation = Joi.object({
   tags: Joi.array().items(Joi.string()).allow(null),
   ratingAvg: Joi.number().min(1).max(5),
   price: Joi.number().min(0),
-  isTopSellingProduct: Joi.boolean(),
+ isTopSellingProduct: Joi.alternatives().try(
+    Joi.boolean(),
+    Joi.string().valid("true", "false")
+  ).optional(),
 });
 
 const getSpecificProductValidation = Joi.object({
@@ -89,7 +92,11 @@ const updateProductValidation = Joi.object({
 
   ratingAvg: Joi.number().min(1).max(5),
   price: Joi.number().min(0),
-  isTopSellingProduct: Joi.boolean(),
+   price: Joi.number().min(0),
+  isTopSellingProduct: Joi.alternatives().try(
+      Joi.boolean(),
+      Joi.string().valid("true", "false")
+    ).optional(),
 });
 
 const deleteProductValidation = Joi.object({
